a4aa3ba1754525dcef46413939b64bf84a565a70
OskarElvkull <oskarelvkull@pvh.com>
2024-01-19 18:40:03 +0100

    added some comments on some complex logic

--- robot-server/src/robot/memory.py
+++ robot-server/src/robot/memory.py
@@ -12,7 +12,21 @@ class BitMapMemory:
         self.unique_visits = 0
 
     def _get_index(self, x, y):
-        return (x + self.width // 2) * self.height + (y + self.height // 2)
+        # NOTE: Need to add some extra logic in this function to make it
+        #       work with our grid, which is centered around 0. 
+
+        # adjust coordinates
+        # Ex. y = 0, then we lift it to 100 000 (center of the grid in the class)
+        y_adjusted = y + self.width // 2 
+        x_adjusted = x + self.height // 2
+
+        # Here we calculate the "location"-pos in the 2d array
+        # To make it easier we leave the adjusted part and do a small array, 3*3.
+        # Ex. [0, 0, 0, | 0, 0, 0, | 0, 0, 0] - each pipe is a COLUMN, so first value is
+        #    (0, 0), (0, 1), (0, 2)
+        # Thats why we need to tage x * height to move to the correct "x"-value, then we
+        # just "climb" to right y by adding y.
+        return x_adjusted * self.height + y_adjusted
 
     def add_location(self, x: int, y: int):
         index = self._get_index(x, y)
@@ -34,7 +48,7 @@ class BitMapMemoryHomeMade:
         self.unique_visits = 0
 
     def _get_index(self, x, y):
-        return (x + self.width // 2) * self.height + (y + self.height // 2)
+        return (x + self.width // 2) * self.width + (y + self.height // 2)
     
     def add_location(self, x: int, y: int):
         index = self._get_index(x, y)
